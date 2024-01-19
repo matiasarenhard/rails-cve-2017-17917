65e591cf422027dc41df6ba47e2bdc65b022b96f
Matias <matias_arenhard@hotmail.com>
2024-01-18 21:40:34 -0300

    feat: create client scaffold

---
+++ db/migrate/20240119002301_create_clients.rb
@@ -0,0 +1,13 @@
+class CreateClients < ActiveRecord::Migration[7.0]
+  def change
+    create_table :clients do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :cpf
+      t.string :phone
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
